dea7541c17fbbde1285fcbe21572d9651d155b09
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-11-29 23:21:03 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-alert("Hello, world!");
-
-const btn = document.querySelector("Bigger");
-
-function random(number) {
-  return Math.floor(Math.random() * (number + 1));
+function onceHandler(event) {
+  alert("Hello, world!");
 }
 
-btn.addEventListener("click", () => {
-  const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
-  document.body.style.backgroundColor = rndCol;
-});
