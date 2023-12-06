eab8fda161674b12bb1bd3d3f9f778018078e88a
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-06 12:03:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const dialog = document.querySelector("dialog");
-const showButton = document.querySelector("dialog + button");
-const closeButton = document.querySelector("dialog button");
-
-// "Show the dialog" button opens the dialog modally
+const Bigger = index.querySelector("Bigger");
+const showButton = index.querySelector("Bigger + button");
 showButton.addEventListener("click", () => {
-  dialog.showModal();
-});
-
-// "Close" button closes the dialog
-closeButton.addEventListener("click", () => {
-  dialog.close();
+  bigger.showAlert();
+  alert("Hello, world!");
 });
