5aa2dc47946cb06c7777992906b1c3561a34ecb2
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-06 11:58:57 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("Hello, world!");
+const Bigger = document.querySelector("Bigger");
+const showButton = document.querySelector("Bigger + button");
+showButton.addEventListener("click", () => {
+  bigger.showAlert();
+  alert("Hello, world!");
+});
+
