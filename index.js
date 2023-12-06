dc4c7fabec4ed7d1fb0bf3bbf27d9c6ae75dbfaa
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-06 13:38:15 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 alert("Hello, world!");
 
+const btn = document.querySelector("#bigger");
+
 function biggerSize() {
   document.getElementById("taid").style.propertyName = "24pt";
 }
+btn.onclick = biggerSize();
 
 
 
