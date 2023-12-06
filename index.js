24bb5b5819ecafc749cfcfb8320861a7aeac5db8
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-06 11:35:10 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const button = document.querySelector("input");
+button.addEventListener("click", updateButton);
+
 function updateButton() {
     if(button.value == "Bigger") {
         alert("Hello, world!");
