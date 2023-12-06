1ce8d0ad666646c5cf6b325fb03fe9d0a48f89fa
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-06 12:06:52 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-const Bigger = index.querySelector("Bigger");
-const showButton = index.querySelector("Bigger + button");
-showButton.addEventListener("click", () => {
-  bigger.showAlert();
-  alert("Hello, world!");
-});
+alert("Hello, world!");
+
