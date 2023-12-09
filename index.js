7bd51ec566dd20cfa72f8c7be0af00108730be2f
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-09 14:00:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ alert("Hello, world!");
 const btn = document.querySelector("button");
 
 btn.onclick = () => {
- document.getElementById("taid").style.propertyName = "4em";
+ document.getElementById("text").style.propertyName = "4em";
 };
 
 
