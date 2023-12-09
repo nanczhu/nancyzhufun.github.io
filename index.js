2bf82c3176a1ac88d2fed35205086d47d7ec31df
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-09 13:05:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 alert("Hello, world!");
 
-const btn = document.querySelector("button");
-
-function text(number) {
-  return fontsize();
-}
-
-btn.addEventListener("click", () => {
- const textsizing = text(24pt);
- document.getElementById("taid").style.propertyName = textsizing;
-});
-
-
-
 
 
