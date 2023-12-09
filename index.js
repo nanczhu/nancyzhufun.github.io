55a63e99b1a252b9cecda7a7840cf2c12b77435b
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-09 14:19:59 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ alert("Hello, world!");
 
 const btn = document.querySelector("button");
 
+
+
 btn.onclick = () => {
- document.getElementById("text").style.propertyName = "4em";
+ document.getElementById("text").style.propertyName = "text";
+  .bigger {
+    font-size: 24pt;
+  }
 };
 
 
