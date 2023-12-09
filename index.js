22fc053a013d554ff9d0ed73c44ba0eb40b5d631
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-09 13:47:00 -0800

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const btn = document.querySelector("button");
 btn.onclick = () => {
  const textChange = fontsize(24);
  document.getElementById("taid").style.propertyName = textChange;
-});
+};
 
 
 
