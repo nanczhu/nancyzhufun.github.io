ee00cbb12d9e826b67d947c912211f17fadb6e71
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-09 12:52:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ alert("Hello, world!");
 const btn = document.querySelector("button");
 
 function changeSize(newText) {
- const text = document.getElementById("taid").style.propertyName = "4em";
+ const text = document.getElementById("taid").style.propertyText = "24pt";
 }
 
 
