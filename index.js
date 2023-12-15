f7dc6d2a8a467e69b2baeceadc01d6587a0d6d06
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-14 23:49:34 -0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const btn = document.querySelector("bid");
 
 function textChange() {
   const value = '24pt';
-  document.getElementById("text").style.propertyName = bid;
+  document.getElementById("text").style.propertyName = value;
 }
 
 btn.onclick = textChange;
