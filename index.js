5500775baa90eb363c30bbb09c2b9bfd27a2ae8a
nanczhu <152056335+nanczhu@users.noreply.github.com>
2023-12-15 16:28:45 -0800

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,21 @@ function textChange() {
 btn.onclick = textChange;
 
 
+if(localStorage.getItem('color-mode'))
+{
+     localStorage.setItem("color-mode", document.getElementById("color-mode"));
+}
+else {
+   setStyle();
+}
+function setStyle() {
+    let rdColor = 'rgb(${random(255)}, ${random(255)}, ${random(255)})';
+    document.body.style.backgroundColor = rdColor;
+    document.getElementById("color-mode").value = rdcolor;
+    htmlElem.style.backgroundColor = '#${rdColor }}';
+    localStorage.removeItem("color-mode");
+}
+
+
 
 
